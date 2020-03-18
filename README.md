072fab3a26e56ce13bf2d2059a17e29fb1579d0f
bruce-robbins <robbinwoodfarm@gmail.com>
2020-03-18 15:49:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5noajh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5noajh)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'; 
+
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive'; // this includes the core NgIdleModule but includes keepalive providers for easy wireup
+
+import { MomentModule } from 'angular2-moment'; // optional, provides moment-style pipes for date formatting
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MomentModule,
+    HttpClientModule,
+    NgIdleKeepaliveModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
